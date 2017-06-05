5744a00b7206a2c3fb8ce93ea42a0f99e1d950df
Dmitry <irvis@imega.ru>
2017-06-05 10:50:30 +0300

    test added term

--- main.go
+++ main.go
@@ -3,16 +3,41 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/gosimple/slug"
 	squirrel "gopkg.in/Masterminds/squirrel.v1"
 )
 
-func main() {
-	builder := squirrel.Insert("terms")
-	builder = builder.Columns("term", "value").Values("1", "2")
-	builder = builder.Values([]byte("A"),"6")
+type Term struct {
+	ID    ID
+	Name  string
+	Slug  Slug
+	Group int
+}
 
-	q, args, err := builder.ToSql()
-	fmt.Println(q, args, err)
+type Slug string
+
+func (s Slug) String() string {
+	return slug.Make(string(s))
+}
+
+type ID int
+
+func (i ID) String() string {
+	return fmt.Sprintf("@max_term_id+%d", i)
+}
+
+func main() {
+	t := Term{
+		ID:   1,
+		Name: "name",
+		Slug: "Имя Фамилия",
+	}
+
+	builder := squirrel.Insert("terms")
+	builder = builder.Columns("term_id", "name", "slug")
+	builder = builder.Values(squirrel.Expr(t.ID.String()), t.Name, t.Slug)
+
 	query := squirrel.DebugSqlizer(builder)
 	fmt.Println(query)
 }
