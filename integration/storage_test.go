08f89b56852ab85e6866d021f7f757985f96cda1
Dmitry <irvis@imega.ru>
2017-06-10 12:49:48 +0300

    add tests group

--- integration/storage_test.go
+++ integration/storage_test.go
@@ -70,3 +70,49 @@ func Test_Groups_ReturnsGroups(t *testing.T) {
 
 	assert.Equal(t, expected, groups)
 }
+
+func Test_Groups_NotExistsGroup_ReturnsEmptyGroups(t *testing.T) {
+	db, teardown := dbUnit.setup(t, "groups", func(db *sql.DB) (err error) {
+		return
+	})
+	defer teardown()
+
+	s := mysql.NewStorage(db)
+	groups, err := s.Groups("")
+	assert.NoError(t, err)
+	expected := []commerceml.Group{}
+
+	assert.Equal(t, expected, groups)
+}
+
+func Test_Groups_WithChildGroup_ReturnsGroups(t *testing.T) {
+	db, teardown := dbUnit.setup(t, "groups", func(db *sql.DB) (err error) {
+		_, err = db.Query("INSERT groups VALUES (?,?,?)", "ecc82696-3f98-11de-991a-001c23888998", "", "Group 1")
+		_, err = db.Query("INSERT groups VALUES (?,?,?)", "7077e5f0-f2a5-11de-bc7e-0022b0527b2e", "ecc82696-3f98-11de-991a-001c23888998", "Child Group 1")
+		return
+	})
+	defer teardown()
+
+	s := mysql.NewStorage(db)
+	groups, err := s.Groups("")
+	assert.NoError(t, err)
+	expected := []commerceml.Group{
+		{
+			IdName: commerceml.IdName{
+				Id:   "ecc82696-3f98-11de-991a-001c23888998",
+				Name: "Group 1",
+			},
+			Groups: []commerceml.Group{
+				{
+					IdName: commerceml.IdName{
+						Id:   "7077e5f0-f2a5-11de-bc7e-0022b0527b2e",
+						Name: "Child Group 1",
+					},
+					Groups: []commerceml.Group{},
+				},
+			},
+		},
+	}
+
+	assert.Equal(t, expected, groups)
+}
